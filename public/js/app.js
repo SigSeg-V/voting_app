d3700ea999f3f2595e59da310f3bab6d8f04f5f6
Charlie <bilzt20@gmail.com>
2022-07-17 12:32:22 +1000

    Added voting system to app

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,84 @@
 
 class ProductList extends React.Component {
-    render = () =>
-        <div className="ui stackable items">
-            <Product/>
-        </div>
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [],
+        }
+    }
+
+    componentDidMount = () => this.setState({products: Seed.products})
+
+    handleProductUpVote = (productId) => {
+        const nextProducts = this.state.products.map((product) =>{
+            if (product.id == productId){
+                return(
+                    Object.assign({}, product, {votes: product.votes + 1})
+                );
+            }
+            else{
+                return product;
+            }
+        });
+        this.setState({products: nextProducts});
+    }
+
+    render = () => {
+        const products = this.state.products.sort((a,b) =>
+        b.votes - a.votes);
+
+        const productComponents =
+            products.map( (product) =>
+                <Product
+                    key = {'product'+product.id}
+                    id={product.id}
+                    title={product.title}
+                    description={product.description}
+                    url={product.url}
+                    votes={product.votes}
+                    submitterAvatarUrl={product.submitterAvatarUrl}
+                    productImageUrl={product.productImageUrl}
+                    onVote={this.handleProductUpVote}
+                />
+            )
+        return (
+        <div className="ui unstackable items">
+            {productComponents}
+        </div>)
+    }
+
 }
 
 class Product extends React.Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.handleUpVote = this.handleUpVote.bind(this)
+    }
+
+    handleUpVote = () => {
+        this.props.onVote(this.props.id);
+    }
+
     render = () =>
     <div className="item">
         <div className="image">
-            <img src="images/products/image-aqua.png" alt="Aqua Image"/>
+            <img src={this.props.productImageUrl} alt="Aqua Image"/>
         </div>
         <div className="middle aligned content">
+            <div className="header">
+                <a onClick={this.handleUpVote}><i className="large caret up icon"></i></a>
+                {this.props.votes}
+            </div>
             <div className="description">
-                <a>Fort Knight</a>
-                <p>Authentic renaissance actors, delivered in just two weeks!</p>
+                <a href={this.props.url}>{this.props.title}</a>
+                <p>{this.props.description}</p>
             </div>
             <div className="extra">
                 <span>Submitted by: </span>
-                <img src="images/avatars/daniel.jpg" className="ui avatar image"/>
+                <img src={this.props.submitterAvatarUrl} className="ui avatar image"/>
             </div>
         </div>
     </div>
